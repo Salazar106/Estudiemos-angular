814e1cc101645d64349a4a1547c241a898596b4f
Daniel Salazar <jdps106@hotmail.com>
2023-11-29 13:24:40 -0500

    Cree componentes y realice un enrutamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes }            from '@angular/router';
+import { LoginComponent }    from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent }     from './components/templates/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {path:'',       title:'Inicio',   component: HomeComponent},
+    {path:'login',  title:'Login',    component: LoginComponent},
+    {path:'sing-up',title:'Register', component: RegisterComponent}
+];
