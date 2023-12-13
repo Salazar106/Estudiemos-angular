4a7b6e1dc5c3798ba0b668ee0037577792fb5d65
Daniel Salazar <jdps106@hotmail.com>
2023-12-13 13:24:58 -0500

    se agregan form de registro

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [CommonModule],
+  selector:    'app-register',
+  standalone:  true,
+  imports:     [CommonModule,ReactiveFormsModule, NgIf],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl:    './register.component.css'
 })
 export class RegisterComponent {
+
+  newUser:FormGroup;
+
+
+  constructor(
+    private formBuilder :FormBuilder,
+    private fb          :FormBuilder
+  ){
+    
+    this.newUser = this.fb.group({
+      firtsName      :['',Validators.required],
+      lastsName      :['',Validators.required],
+      username       :['',Validators.required],
+      email          :['',Validators.required],
+      password       :['',Validators.required],
+      confirmPassword:['',Validators.required],
+    })
+  }
+
+  RegisterUser(){
+    console.log(this.newUser.value);
+  }
 
 }
